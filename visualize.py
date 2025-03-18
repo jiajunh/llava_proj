1560ce6d45330c6b50c05789fac3f57e57cc88c8
jiajunh <jiajunh@umich.edu>
2025-03-18 15:57:23 -0400

    test deploy

--- visualize.py
+++ visualize.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 from utils import get_one_image
 
-img_path = get_one_image()
+img_path = get_one_image(image_path="test_data")
 
 st.set_page_config(page_title="Hello", page_icon=":material/waving_hand:")
 st.title("Welcome to Streamlit!")
