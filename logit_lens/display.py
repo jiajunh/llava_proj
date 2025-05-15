5caae1b868dca8aa8f92f5a86852c03785a04105
jiajunh <jiajunh@umich.edu>
2025-05-15 15:17:32 -0400

    font and patch

--- logit_lens/display.py
+++ logit_lens/display.py
@@ -96,12 +96,13 @@ class LogitLensVisualizer:
         print(np.min(cam), np.max(cam), np.mean(cam))
         cam_img = np.uint8(255 * cam)
         print(np.min(cam_img), np.max(cam_img), np.mean(cam_img))
-        print(cam_img.shape)
         heatmap = [cv2.applyColorMap(cam_img_head, cv2.COLORMAP_HSV) for cam_img_head in cam_img]
         heatmap = np.array(heatmap)
         heatmap = np.float32(heatmap) / 255
 
-        print(heatmap.shape)
+        print(np.min(heatmap[0,:,:,0]), np.max(heatmap[0,:,:,0]), np.mean(heatmap[0,:,:,0]))
+        print(np.min(heatmap[0,:,:,1]), np.max(heatmap[0,:,:,1]), np.mean(heatmap[0,:,:,1]))
+        print(np.min(heatmap[0,:,:,2]), np.max(heatmap[0,:,:,2]), np.mean(heatmap[0,:,:,2]))
 
         # mul = 1.0
         # cam = atten_map
